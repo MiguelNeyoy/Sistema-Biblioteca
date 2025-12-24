4ebfa0fcaffed28f8e53ee09cb5f79a5fd350be3
MiguelNeyoy <miguelneyoycn@gmail.com>
2025-12-23 21:29:26 -0700

    Se concluyo la clase libro y se avanzo la clase biblioteca

--- Sistema-De-Gestion-De-Biblioteca/Biblioteca.cs
+++ Sistema-De-Gestion-De-Biblioteca/Biblioteca.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ *  La funcion de esta clase es de gestionar la biblioteca con los siguientes metodos:
+ *  
+ *  -Alamcenar libros
+ *  -Acceder a los libros
+ *  -Eliminar libros
+ *  -Modificar un libro
+ *  
+ */
 namespace Sistema_De_Gestion_De_Biblioteca
 {
     internal class Biblioteca
     {
+        private List<Libro> ListaDeLibros { get; set; }
+        public Biblioteca()
+        {
+            ListaDeLibros = new List<Libro>();
+        }
+
+        public void AlmacenarLibro(Libro libro)
+        {
+            ListaDeLibros.Add(libro);
+        }
+
+        public List<Libro> AccederLibros()
+        {
+            return ListaDeLibros;
+        }
+
     }
 }
--- Sistema-De-Gestion-De-Biblioteca/Libro.cs
+++ Sistema-De-Gestion-De-Biblioteca/Libro.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * 
+ *  Esta clase libro se creo con el objetivo de crear el objeto libro, con los siguientes metodos:
+ *  -Guardar Libro (Este metodo exporta los datos necesarios para almacenar en la clase Biblioteca)
+ *  -
+ * 
+ */
 namespace Sistema_De_Gestion_De_Biblioteca
 {
     internal class Libro
